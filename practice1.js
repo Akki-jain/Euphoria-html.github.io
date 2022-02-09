fe44533a775bd4e56dcce4cf3c9c6cb162d91713
Akshat Jain <jainakshat75@gmail.com>
2022-02-09 08:35:32 +0530

    Added all the files

---
+++ practice1.js
@@ -0,0 +1,34 @@
+alert("message");
+comfirm("message");
+prompt("message");
+
+var name="Akshat";
+var age=32;
+var weight=58.6;
+var credits=2+4+(2*3);
+
+//this is a comment
+/* This is also a comment */
+
+var three=Math.floor(Math.PI);
+var rand=Math.floor(Math.random())
+
+var ned=NULL;
+
+var sum=0;
+for(var i=0; i<100; i++)
+{
+    sum=sum+i;
+}
+
+var ducks=["Huey", "Dewey", "Louie"];
+var stooges=[];
+stooges[0]="Hello";
+stooges[1]="World";
+
+var a=["Stef","Jason"];
+a.push("Hello");
+a.unshift("World");
+a.pop();
+a.shift();
+a.sort();
